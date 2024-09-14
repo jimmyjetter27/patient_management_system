fc6a37702bf59f668523efc946938740baecc88c
James Atiah <jimmyjetter27@gmail.com>
2024-09-14 13:33:16 +0000

    working on add service feature

--- resources/js/Pages/Patients/Patients.jsx
+++ resources/js/Pages/Patients/Patients.jsx
@@ -234,6 +234,18 @@ const Patient = ({auth}) => {
             cell: (row) => (
                 <div className="flex justify-center space-x-2">
                     <div>
+                        <button className="text-green-600 hover:text-green-900"
+                            onClick={() => confirmPatientUpdate({
+                                id: row.id,
+                                name: row.name,
+                            })}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div>
                         <button
                             onClick={() => confirmPatientUpdate({
                                 id: row.id,
